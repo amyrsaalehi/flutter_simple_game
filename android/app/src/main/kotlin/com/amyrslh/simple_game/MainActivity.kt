a95d958f95cf8cb04cd431d3591da085dcd9b642
amyrsaalehi <salehi.amir9@gmail.com>
2021-03-12 08:15:21 -0800

    Finished

---
+++ android/app/src/main/kotlin/com/amyrslh/simple_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amyrslh.simple_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'App.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Game',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: App(title: 'Game'),
+    );
+  }
+}
